079f6375b9aff34bbb04f6810798eabf5ede28fe
Brian Guerrero <13bguerrero@gmail.com>
2025-02-05 22:23:22 -0600

    Added property to diagram models.

--- DotUML.CLI/Models/DiagramModels.cs
+++ DotUML.CLI/Models/DiagramModels.cs
@@ -2,9 +2,17 @@ using System.Text;
 
 namespace DotUML.CLI.Models;
 
+public record PropertyInfo(string Name, string Visibility, string Type)
+{
+    public string GetDiagramRepresentation() => $"        {Visibility}{Name} : {Type}";
+}
+
 public abstract record ObjectInfo(string Name)
 {
+    protected List<PropertyInfo> _properties = new();
     public abstract string GetDiagramRepresentation();
+
+    public void AddProperty(PropertyInfo property) => _properties.Add(property);
 }
 
 public record ClassInfo(string Name, string? BaseClass = "") : ObjectInfo(Name)
@@ -12,8 +20,9 @@ public record ClassInfo(string Name, string? BaseClass = "") : ObjectInfo(Name)
     public override string GetDiagramRepresentation()
     {
         var sb = new StringBuilder();
-        sb.AppendLine($"    class {Name} {{ }}");
-        if (string.IsNullOrEmpty(BaseClass))
+        sb.AppendLine($"    class {Name} {{");
+        sb.AppendJoin(string.Empty, _properties.Select(p => p.GetDiagramRepresentation()));
+        sb.AppendLine("    }"); if (string.IsNullOrEmpty(BaseClass))
         {
             return sb.ToString();
         }
@@ -29,6 +38,7 @@ public record InterfaceInfo(string Name) : ObjectInfo(Name)
         var sb = new StringBuilder();
         sb.AppendLine($"    class {Name} {{");
         sb.AppendLine("        <<interface>>");
+        sb.AppendJoin(string.Empty, _properties.Select(p => p.GetDiagramRepresentation()));
         sb.AppendLine("    }");
         return sb.ToString();
     }
