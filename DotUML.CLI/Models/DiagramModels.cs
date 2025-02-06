adb4d06ce3b9cfd7be7a44e9cca44cbb96808889
Brian Guerrero <13bguerrero@gmail.com>
2025-02-05 23:04:33 -0600

    Add MethodInfo record and update diagram models to support method representation

--- DotUML.CLI/Models/DiagramModels.cs
+++ DotUML.CLI/Models/DiagramModels.cs
@@ -14,12 +14,35 @@ public record PropertyInfo(string Name, string Visibility, string Type)
     public string GetDiagramRepresentation() => $"        {VisibilityCharacter}{Name} : {Type}\n";
 }
 
+public record MethodInfo(string Name, string Visibility, string? ReturnType = "")
+{
+    private char VisibilityCharacter => Visibility switch
+    {
+        var v when v.Contains("public") => '+',
+        var v when v.Contains("protected") => '#',
+        var v when v.Contains("private") => '-',
+        _ => '?'
+    };
+    public string GetDiagramRepresentation()
+    {
+        if (string.IsNullOrEmpty(ReturnType))
+        {
+            return $"        {VisibilityCharacter}{Name}()\n";
+        }
+        return $"        {VisibilityCharacter}{Name}() : {ReturnType}\n";
+    }
+}
+
 public abstract record ObjectInfo(string Name)
 {
     protected List<PropertyInfo> _properties = new();
+    protected List<MethodInfo> _methods = new();
+
     public abstract string GetDiagramRepresentation();
 
     public void AddProperty(PropertyInfo property) => _properties.Add(property);
+    public void AddMethod(MethodInfo method) => _methods.Add(method);
+
 }
 
 public record ClassInfo(string Name, string? BaseClass = "") : ObjectInfo(Name)
@@ -29,6 +52,7 @@ public record ClassInfo(string Name, string? BaseClass = "") : ObjectInfo(Name)
         var sb = new StringBuilder();
         sb.AppendLine($"    class {Name} {{");
         sb.AppendJoin(string.Empty, _properties.Select(p => p.GetDiagramRepresentation()));
+        sb.AppendJoin(string.Empty, _methods.Select(p => p.GetDiagramRepresentation()));
         sb.AppendLine("    }"); if (string.IsNullOrEmpty(BaseClass))
         {
             return sb.ToString();
@@ -46,6 +70,7 @@ public record InterfaceInfo(string Name) : ObjectInfo(Name)
         sb.AppendLine($"    class {Name} {{");
         sb.AppendLine("        <<interface>>");
         sb.AppendJoin(string.Empty, _properties.Select(p => p.GetDiagramRepresentation()));
+        sb.AppendJoin(string.Empty, _methods.Select(p => p.GetDiagramRepresentation()));
         sb.AppendLine("    }");
         return sb.ToString();
     }
