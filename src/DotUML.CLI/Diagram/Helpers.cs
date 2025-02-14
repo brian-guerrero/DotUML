05a23d2a5d472bd08999c5ccb8155a28d7299cd4
Brian Guerrero <13bguerrero@gmail.com>
2025-02-14 16:38:37 -0600

    Enhance visibility character retrieval to include internal access modifier and return string

--- src/DotUML.CLI/Diagram/Helpers.cs
+++ src/DotUML.CLI/Diagram/Helpers.cs
@@ -3,11 +3,12 @@ namespace DotUML.CLI.Diagram;
 // Utility method to get visibility character
 public static class Helpers
 {
-    public static char GetVisibilityCharacter(string visibility) => visibility switch
+    public static string GetVisibilityCharacter(string visibility) => visibility switch
     {
-        var v when v.Contains("public") => '+',
-        var v when v.Contains("protected") => '#',
-        var v when v.Contains("private") => '-',
-        _ => '?'
+        var v when v.Contains("public") => "+",
+        var v when v.Contains("protected") => "#",
+        var v when v.Contains("private") => "-",
+        var v when v.Contains("internal") => "~",
+        _ => string.Empty
     };
 }
